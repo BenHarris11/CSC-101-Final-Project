bb9e4d2a5769c98b61496c79762779d6e06af662
Ben Harris <bharri44@calpoly.edu>
2025-02-26 17:26:53 -0800

    Blue Print

--- main.py
+++ main.py
@@ -1,17 +1,62 @@
-# This is a sample Python script.
+class Bank:
+    def __init__(self):
+        self.customers = {}  # Dictionary of Customer objects
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def create_customer(self, name, customer_id):
+        """Creates a new customer and adds to the bank."""
+        pass
+
+    def close_account(self, account_number):
+        """Closes an account and stores its details."""
+        pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Customer:
+    def __init__(self, name, customer_id):
+        self.name = name
+        self.customer_id = customer_id
+        self.accounts = {}  # Dictionary of Account objects
+
+    def create_account(self, account_number, account_type):
+        """Creates a new bank account for the customer."""
+        pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Account:
+    def __init__(self, account_number, account_type, balance=0.0):
+        self.account_number = account_number
+        self.account_type = account_type  # e.g., "Checking", "Savings"
+        self.balance = balance
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print('test')
+    def deposit(self, amount):
+        """Deposits money into the account."""
+        pass
+
+    def withdraw(self, amount):
+        """Withdraws money if sufficient balance is available."""
+        pass
+
+    def transfer(self, target_account, amount):
+        """Transfers money to another account."""
+        pass
+
+
+class Transaction:
+    def __init__(self, transaction_type, amount, account_number, target_account=None):
+        self.transaction_type = transaction_type  # "Deposit", "Withdrawal", "Transfer"
+        self.amount = amount
+        self.account_number = account_number
+        self.target_account = target_account
+
+    def process_transaction(self):
+        """Processes the transaction and updates accounts."""
+        pass
+
+
+class Receipt:
+    def __init__(self, transaction):
+        self.transaction = transaction
+
+    def generate_receipt(self):
+        """Generates a transaction receipt."""
+        pass
