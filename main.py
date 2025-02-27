23600be243b912eb55767971d7072b6aea13dfc0
Ben Harris <bharri44@calpoly.edu>
2025-02-26 17:48:20 -0800

    Blue Print.3

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ class Customer:
         self.customer_id = customer_id
         self.accounts = {}  # Dictionary of Account objects
 
-    def create_account(self, account_number, account_type):
-        """Creates a new bank account for the customer."""
-
-
 
 class Account:
     def __init__(self, account_number, account_type, balance=0.0):
@@ -15,8 +11,11 @@ class Account:
         self.account_type = account_type  # e.g., "Checking", "Savings"
         self.balance = balance
 
+    def create_account(self, account_number, account_type):
+        """Creates a new bank account for the customer."""
 
-
+    def close_account(self, account_number, account_type):
+        """Closes a bank account for the customer."""
 
 class Transaction:
     def __init__(self, transaction_type, amount, account_number, target_account=None):
