ef74dbba4b2ba40f958815f6dab5429a83ca3608
Karsten <karstents19@gmail.com>
2025-03-05 18:12:12 -0800

    3/5/25 Progress

--- Accounts.py
+++ Accounts.py
@@ -11,10 +11,17 @@ class Account:
 # The master list of all account objects within our system
 account_list = []
 
+# Creates a new Account object and appends it to our account_list
+# inputs: All the attributes for a new Account object, with the same data types
+# output: None, just appends the account_list
 def create_account(customer_id: int, account_number: int, account_type: str, balance: float)-> None:
     account_list.append(Account(customer_id, account_number, account_type, balance))
 
-def close_account(customer_id: int, account_number: int):
+# Removes an existing Account object from our account_list and returns an Error message
+# if the account is not found
+# inputs: the customer id & account number for the account to be deleted
+# output: None, either updates the account_list or prints an error message
+def close_account(customer_id: int, account_number: int)-> None:
     target = None
     for acct in account_list:
         if customer_id == acct.customer_id and account_number == acct.account_number:
--- Recipts.py
+++ Recipts.py
@@ -1,6 +1,9 @@
+from Transactions import Transaction
+
 class Receipt:
-    def __init__(self, transaction):
+    def __init__(self, transaction: Transaction):
         self.transaction = transaction
 
+#
     def generate_receipt(self):
         """Generates a transaction receipt."""
