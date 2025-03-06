839a4186f74d354ba7ac7e380e81b9b0a19f1da6
Ben Harris <bharri44@calpoly.edu>
2025-03-05 17:15:17 -0800

    Separated Classes

--- Accounts.py
+++ Accounts.py
@@ -1,11 +1,25 @@
 class Account:
-    def __init__(self, account_number, account_type, balance=0.0):
+    def __init__(self, customer_id: int, account_number: int, account_type: str, balance: float):
+        self.customer_id = customer_id
         self.account_number = account_number
         self.account_type = account_type  # e.g., "Checking", "Savings"
         self.balance = balance
+    def __repr__(self)-> str:
+        return ('Account: (Customer Id: {}, Account Num: {}, Account Type: {}, Balance: {})'
+                .format(self.customer_id, self.account_number, self.account_type, self.balance))
 
-    def create_account(self, account_number, account_type):
-        """Creates a new bank account for the customer."""
+# The master list of all account objects within our system
+account_list = []
 
-    def close_account(self, account_number, account_type):
-        """Closes a bank account for the customer."""
+def create_account(customer_id: int, account_number: int, account_type: str, balance: float)-> None:
+    account_list.append(Account(customer_id, account_number, account_type, balance))
+
+def close_account(customer_id: int, account_number: int):
+    target = None
+    for acct in account_list:
+        if customer_id == acct.customer_id and account_number == acct.account_number:
+            target = acct
+    if target:
+        account_list.remove(target)
+    else:
+        print("Account Not Found")
--- Customer.py
+++ Customer.py
@@ -1,5 +1,9 @@
 class Customer:
-    def __init__(self, name, customer_id):
+    def __init__(self, name: str, customer_id: int, cust_accounts: {Account}):
         self.name = name
         self.customer_id = customer_id
-        self.accounts = {}  # Dictionary of Account objects
+        self.cust_accounts = cust_accounts  # Dictionary of Account objects
+    def __repr__(self)-> str:
+        return ('Customer: (Name: {}, Customer Id: {}, Accounts: {}'
+                .format(self.name, self.customer_id, self.cust_accounts))
+
