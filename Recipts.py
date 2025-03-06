52d455b65e5e1584a457a3969f404776fc59f275
Ben Harris <bharri44@calpoly.edu>
2025-03-05 18:21:52 -0800

    added Master List of Customers

--- Recipts.py
+++ Recipts.py
@@ -4,6 +4,8 @@ class Receipt:
     def __init__(self, transaction: Transaction):
         self.transaction = transaction
 
-#
+    # Creates a receipt based on the current transaction by the customer on the account
+    # No input
+    # Output: All information associated with transaction by customer
     def generate_receipt(self):
         """Generates a transaction receipt."""
